b0ac7b0b779b2d4fe02e6a5b9068353e2e60c66d
NizzzaN <125788827+NizzzaN@users.noreply.github.com>
2024-01-29 00:15:55 +0100

    P2300709003.py

--- P2300709003.py
+++ P2300709003.py
@@ -74,14 +74,14 @@ def plot_diagrams(time, velocities, distances,file_name_out):
     plt.figure(figsize=(12, 6))
 
     plt.subplot(1, 2, 1)
-    plt.plot(time, velocities)
-    plt.title('Velocity')
+    plt.plot(time, velocities, color ="green", lw = 2)
+    plt.title('Velocity/time', fontweight ="bold")
     plt.xlabel('Time (s)')
     plt.ylabel('Velocity (m/s)')
 
     plt.subplot(1, 2, 2)
-    plt.plot(time, distances)
-    plt.title('Braking Distance')
+    plt.plot(time, distances, color ="red", lw = 2)
+    plt.title('Braking Distance', fontweight ="bold")
     plt.xlabel('Time (s)')
     plt.ylabel('Total Distance (m)')
 
